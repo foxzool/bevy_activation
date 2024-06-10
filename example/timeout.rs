d1fd8a878144c03d8f15a6c7aa555a8c95772a77
ZoOL <zhooul@gmail.com>
2024-06-10 14:20:53 +0800

    project init

---
+++ example/timeout.rs
@@ -0,0 +1,57 @@
+use std::time::Duration;
+use bevy::log::LogPlugin;
+use bevy::prelude::*;
+use bevy_time::common_conditions::on_timer;
+use bevy_activation::{ActivationPlugin, ActiveState, TimeoutEvent};
+
+fn main() {
+    App::new().add_plugins(MinimalPlugins.set(bevy::app::ScheduleRunnerPlugin::run_loop(
+        Duration::from_secs_f64(1.0 / 60.0),
+    )))
+        .add_plugins(LogPlugin::default())
+        .add_plugins(ActivationPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (check_active, timeout_event, reactive_idle.run_if(time_passed(5.0))))
+        .run();
+}
+
+
+#[derive(Component)]
+struct TestAlive(pub &'static str);
+
+fn setup(mut commands: Commands) {
+    // this entity always active
+    commands.spawn((TestAlive("always alive component"), ActiveState::default()));
+    // this entity will be active for 2 seconds
+    commands.spawn((TestAlive("ttl 2 secs component"), ActiveState::new(Duration::from_secs(2))));
+}
+
+fn check_active(q: Query<(&TestAlive, &ActiveState)>) {
+    for (TestAlive(name), active_state) in q.iter() {
+        info!("'{}'  active: {}", name, active_state.is_active());
+    }
+}
+
+fn timeout_event(mut timeout_event: EventReader<TimeoutEvent>) {
+    for timeout_ev in timeout_event.read() {
+        warn!("entity {:?} is idle", timeout_ev.0);
+    }
+}
+
+fn reactive_idle(mut q_idle: Query<&mut ActiveState>) {
+    for mut active_state in q_idle.iter_mut() {
+        if active_state.is_idle() {
+            active_state.toggle();
+        }
+    }
+}
+
+
+fn time_passed(t: f32) -> impl FnMut(Local<f32>, Res<Time>) -> bool {
+    move |mut timer: Local<f32>, time: Res<Time>| {
+        // Tick the timer
+        *timer += time.delta_seconds();
+        // Return true if the timer has passed the time
+        *timer >= t
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,122 @@
+use std::time::Duration;
+use bevy_reflect::Reflect;
+use bevy_time::{Stopwatch, Time};
+use bevy_app::{App, Plugin, PostUpdate};
+use bevy_derive::Deref;
+use bevy_ecs::prelude::*;
+
+
+/// Bevy Entity Activation Plugin;
+pub struct ActivationPlugin;
+
+impl Plugin for ActivationPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_type::<ActiveState>()
+            .add_event::<TimeoutEvent>()
+            .add_systems(PostUpdate, check_timeout);
+    }
+}
+
+/// Timeout Event
+#[derive(Deref, Debug, Event)]
+pub struct TimeoutEvent(pub Entity);
+
+/// Activation State Component
+#[derive(Debug, Component, Reflect)]
+#[reflect(Component)]
+pub struct ActiveState {
+    /// is active
+    active: bool,
+    /// stop watch
+    watch: Option<Stopwatch>,
+    /// Time to Interactive (TTI)
+    time_to_idle: Duration,
+}
+
+impl Default for ActiveState {
+    fn default() -> Self {
+        Self::always()
+    }
+}
+
+impl ActiveState {
+    /// no timeout
+    pub fn always() -> ActiveState {
+        ActiveState {
+            active: true,
+            watch: None,
+            time_to_idle: Duration::MAX,
+        }
+    }
+
+    #[inline]
+    pub fn is_active(&self) -> bool {
+        self.active
+    }
+
+    #[inline]
+    pub fn is_idle(&self) -> bool {
+        !self.is_active()
+    }
+
+    /// set active
+    #[inline]
+    pub fn active_mut(&mut self) -> &mut bool {
+        &mut self.active
+    }
+
+    /// setup timeout time
+    #[inline]
+    pub fn time_to_idle_mut(&mut self) -> &mut Duration {
+        &mut self.time_to_idle
+    }
+
+    /// new active state with timeout
+    pub fn new(timeout: Duration) -> ActiveState {
+        ActiveState {
+            active: true,
+            watch: Some(Stopwatch::new()),
+            time_to_idle: timeout,
+        }
+    }
+
+    /// set timeout time
+    pub fn set_tti_time(&mut self, timeout: Duration) -> &mut ActiveState {
+        self.time_to_idle = timeout;
+        if self.watch.is_none() {
+            self.watch = Some(Stopwatch::new());
+        }
+        self
+    }
+
+    /// toggle active state
+    pub fn toggle(&mut self) {
+        self.active = true;
+        if let Some(watch) = &mut self.watch {
+            watch.reset();
+        }
+    }
+}
+
+/// set timeout to not active and send timeout event
+fn check_timeout(
+    time: Res<Time>,
+    mut active_state_query: Query<(Entity, &mut ActiveState)>,
+    mut timeout_events: EventWriter<TimeoutEvent>,
+) {
+    for (entity, mut active_state) in active_state_query.iter_mut() {
+        if active_state.is_idle() {
+            continue;
+        }
+        let timeout = active_state.time_to_idle;
+        if let Some(watch) = &mut active_state.watch {
+            watch.tick(time.delta());
+
+            if watch.elapsed() >= timeout {
+                timeout_events.send(TimeoutEvent(entity));
+                watch.reset();
+                active_state.active = false;
+            }
+        }
+    }
+}
