30521c9dca0f5f1c58124d43b0d2ff2845c2c055
ZoOL <zhooul@gmail.com>
2024-06-11 14:26:25 +0800

    update ci

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../README.md")]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 
 use bevy_app::{App, Plugin, PostUpdate};
 use bevy_derive::Deref;
